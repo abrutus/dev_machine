4ef1bf3f3461e37c07da253dbd2e390c8ac71ef3
Andre Brutus <anebg.net@gmail.com>
2012-05-23 14:51:23 -0700

    Initial Setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Optionally, use apt-fast instead of apt-get
+apt-get install pidgin flashplugin-installer vlc chromium-browser mysql-server git p7zip compizconfig-settings-manager openjdk-7-jre rar unrar tomboy vim g++ phpmyadmin python-django python-mysqldb sendmail
+
+# Sublime Text Editor
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text-2-beta
+# https://wiki.ubuntu.com/Bumblebee
+# For switchable graphics
+sudo add-apt-repository ppa:bumblebee/stable
+sudo apt-get update
+apt-get install bumblebee bumblebee-nvidia
