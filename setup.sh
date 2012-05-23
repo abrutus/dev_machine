bfbf3156e7f13fe5aa7bef941c826e6c2917b90f
Andre Brutus <anebg.net@gmail.com>
2012-05-23 14:59:54 -0700

    Commented out switchable graphics

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-# Optionally, use apt-fast instead of apt-get
+#!/bin/bash
+# Optionally, use apt-fast instead of apt-get (find and replace apt-get for apt-fast)
 apt-get install pidgin flashplugin-installer vlc chromium-browser mysql-server git p7zip compizconfig-settings-manager openjdk-7-jre rar unrar tomboy vim g++ phpmyadmin python-django python-mysqldb sendmail
 
 # Sublime Text Editor
@@ -6,7 +7,7 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-2
 sudo apt-get update
 sudo apt-get install sublime-text-2-beta
 # https://wiki.ubuntu.com/Bumblebee
-# For switchable graphics
-sudo add-apt-repository ppa:bumblebee/stable
-sudo apt-get update
-apt-get install bumblebee bumblebee-nvidia
+# For switchable graphics ASUS U45JC-A1
+#sudo add-apt-repository ppa:bumblebee/stable
+#sudo apt-get update
+#apt-get install bumblebee bumblebee-nvidia
